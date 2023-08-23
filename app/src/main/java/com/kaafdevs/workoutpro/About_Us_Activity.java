839e73622f9f1d262011e67e42c5c5ccdb4dff4f
Muhammad Awais Shafi <awaisshafi6164@gmail.com>
2023-08-23 20:35:14 +0500

    Just Checking that github is working or not It is same as initial one

--- app/src/main/java/com/kaafdevs/workoutpro/About_Us_Activity.java
+++ app/src/main/java/com/kaafdevs/workoutpro/About_Us_Activity.java
@@ -10,5 +10,6 @@ public class About_Us_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        //
     }
 }
